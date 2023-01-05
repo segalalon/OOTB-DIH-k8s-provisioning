7bd682bcb7b8bfa3aeaea5b2ad982a635a158f70
Cloud User <shmulik.kaufman@gigaspaces.com>
2023-01-05 18:14:34 +0200

    added helm repo and helm update

--- scripts/install-dih-umbrella.sh
+++ scripts/install-dih-umbrella.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Deploying xap umbrella
+source ../setAWSEnv.sh
+helm repo add gigaspaces https://resources.gigaspaces.com/helm-charts
+helm repo update
 helm install xap gigaspaces/xap --version=16.2.1 --set license="Product=InsightEdge;Version=16.2;Type=ENTERPRISE;Customer=Gigaspaces_K8sAWSEnvTraining_DEV;Expiration=2025-Jan-01;Hash=PPSPYPQcOrQZvNSfORgd"
 
 ./install-k8s-dashboard.sh
@@ -17,3 +20,5 @@ echo "It will take a while for the load balancer to be available ..."
 #   echo -e $(date) " Ops Manager HTTP state: " $(/usr/bin/curl -s -o /dev/null -w %{http_code} ${MANAGER_URL}) " (waiting for 200)"
 #   sleep 3
 # done
+
+
