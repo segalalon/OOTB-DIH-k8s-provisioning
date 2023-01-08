84da785a91a6469cc721b3b2a3d608c164e9342d
Cloud User <shmulik.kaufman@gigaspaces.com>
2023-01-08 15:38:58 +0200

    destroy-eks-lab.sh updated

--- scripts/destroy-eks-lab.sh
+++ scripts/destroy-eks-lab.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+read -r -p "Do you want to destroy your EKS lab ?[y/n] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        
+        ;;
+    *)
+        echo "Aborted."
+        exit
+        ;;
+esac
 cd ../terraform
-terraform destroy
+terraform plan -destroy -out destroy.out
+terraform apply "destroy.out"
 
