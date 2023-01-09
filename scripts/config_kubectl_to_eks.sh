aeda1cbf55561d5313033e599ba1e66f3c49fc90
Cloud User <shmulik.kaufman@gigaspaces.com>
2023-01-09 23:24:01 +0200

    config_kubectl_to_eks.sh updated

--- scripts/config_kubectl_to_eks.sh
+++ scripts/config_kubectl_to_eks.sh
@@ -2,10 +2,12 @@
 # Configure aws and kubectl 
 source ../setAWSEnv.sh
 aws eks update-kubeconfig --name `cat ../clusterName.txt`
+kubectl get svc
 echo
-echo "To config your kubectl to connect your cluster please run:"
-echo " aws eks update-kubeconfig --name `cat ../clusterName.txt`"
+echo
+echo "If you need to reconfig your kubectl to connect your cluster, please run:"
+echo "source setAWSEnv.sh"
+echo "aws eks update-kubeconfig --name `cat ../clusterName.txt`"
 echo
 echo "Test it by running: kubectl get svc"
-echo 
-echo "Your cluster name is stored in clusterName.txt"
+echo ">> Your cluster name is stored in clusterName.txt"
