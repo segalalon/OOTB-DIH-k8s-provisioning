9c70091fd6a0779671e85b635c2c23983a4b6288
Cloud User <shmulik.kaufman@gigaspaces.com>
2023-01-28 22:13:17 +0200

    Ls fixes, scripts updates

--- scripts/install-dih-umbrella.sh
+++ scripts/install-dih-umbrella.sh
@@ -7,17 +7,24 @@ cd $SCRIPTPATH
 source ../setAWSEnv.sh
 helm repo add gigaspaces https://resources.gigaspaces.com/helm-charts
 helm repo update
-helm install xap gigaspaces/xap --version=16.2.1 --set license="Product=InsightEdge;Version=16.2;Type=ENTERPRISE;Customer=Gigaspaces_K8sAWSEnvTraining_DEV;Expiration=2025-Jan-01;Hash=PPSPYPQcOrQZvNSfORgd"
+helm install xap gigaspaces/xap --version=16.2.1 -f ../yaml/gigaspaces.yaml
 
 ./install-k8s-dashboard.sh
 ./get-k8s-dashboard-token.sh
 kubectl apply -f ../yaml/grafana-lb.yaml
 #kubectl apply -f ../yaml/managers-lb.yaml
 
-# Annotate ops-manager LoadBalancer
+# Annotate Load Balancers
 cluster_name=$(cat ../clusterName.txt)
 ops_manager_annotate="kubectl patch svc xap-xap-manager-service -p '{\"metadata\":{\"annotations\":{\"service.beta.kubernetes.io/aws-load-balancer-additional-resource-tags\":\"Owner=$cluster_name, Project=$cluster_name, Name=$cluster_name-opsManager-LB\"}}}'"
 eval  $ops_manager_annotate
+
+k8s_dashboard_annotate="kubectl patch svc k8s-dashboard-lb -n kubernetes-dashboard -p '{\"metadata\":{\"annotations\":{\"service.beta.kubernetes.io/aws-load-balancer-additional-resource-tags\":\"Owner=$cluster_name, Project=$cluster_name, Name=$cluster_name-k8s-dashboard-LB\"}}}'"
+eval  $k8s_dashboard_annotate
+
+grafana_lb_annotate="kubectl patch svc grafana-lb -p '{\"metadata\":{\"annotations\":{\"service.beta.kubernetes.io/aws-load-balancer-additional-resource-tags\":\"Owner=$cluster_name, Project=$cluster_name, Name=$cluster_name-grafana-LB\"}}}'"
+eval  $grafana_lb_annotate
+
 
 ./get-ui-urls.sh
 echo "It will take a while for the load balancer to be available ..."
